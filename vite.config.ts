4566ad57b2a8f2d3f23411570e54a63fa654c9cc
unknown <m62443444g@gmail.com>
2022-12-29 07:53:18 +0330

    update-vite's 'base' options for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Puzzel/',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
